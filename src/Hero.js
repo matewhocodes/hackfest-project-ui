a8520c22f22f365dca25e412cbd0583637eda695
hritik <86347286+hritikd3@users.noreply.github.com>
2022-06-04 15:08:38 +0530

    testing changes

--- src/Hero.js
+++ src/Hero.js
@@ -10,7 +10,7 @@ const Hero = () => {
         <article className="hero-info">
           <h1>
             Payments infrastructure <br />
-            for the internet
+            for the internet..
           </h1>
           <p>
             Millions of companies of all sizes—from startups to Fortune 500s—use
